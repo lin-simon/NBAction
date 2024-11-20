1bd28fb9ab6f94a892aff2bf19972f55f89a5478
lin-simon <lin.simon2003@gmail.com>
2024-11-20 05:31:30 -0500

    improve frame state logic

--- NBAction/nbaction.py
+++ NBAction/nbaction.py
@@ -2,44 +2,50 @@ from ultralytics import YOLO
 import cv2
 import math
 import numpy as np
-from processing import score, in_hoop_region, clean_hoop_pos, clean_ball_pos
+from processing import score, within_shot_radius, stabilize_hoop, stabilize_ball
 
 
 class NBAction:
     def __init__(self):
 
         self.model = YOLO("runs/detect/train14/weights/best.pt")
-        self.class_names = ['Basketball', 'Basketball Hoop', 'Defence', 'Player', 'shooting']
-        self.shots_made = 0  
-        self.ball_in_hoop = False 
-        self.cap = cv2.VideoCapture("testset/trim2.mp4")
-        self.cooldown_frames = 500
-        self.last_attempt_frame = -self.cooldown_frames 
-        self.ball_pos = []  
-        self.hoop_pos = []  
+        self.classes = ['Basketball', 'Basketball Hoop', 'Defence', 'Player', 'shooting']
+        self.video = cv2.VideoCapture("testset/trim4.mp4")
+       
+       
+        self.current_frame = None
+        self.cooldown_current_frames = 500
+        self.last_attempt_current_frame = -self.cooldown_current_frames
+         
+        #(X,Y) Coordinates for current ball and hoop centers
+        self.ball = []  
+        self.hoop = []  
 
-        self.frame_count = 0
-        self.frame = None
-        
-        self.show_score_text = False
+        #Current arc of ball
         self.up = False
         self.down = False
-        self.up_frame = 0
-        self.down_frame = 0
-        self.font = cv2.FONT_HERSHEY_DUPLEX
-        self.fade_frames = 20
-        self.fade_counter = 0
+        self.ball_in_hoop = False 
+        
+        self.show_score_text = False
+        self.frame_count = 0
+        self.total = 0
+        self.shots_made = 0  
+        self.up_current_frame = 0
+        self.down_current_frame = 0
+        self.revert_frames = 20
+
         self.overlay_color = (0, 0, 0)
+        self.font = cv2.FONT_HERSHEY_DUPLEX
+        #Declare window and allow to be resized
         cv2.namedWindow('NBAction', cv2.NORM_MINMAX)
         self.run()
 
     def run(self):
         while True:
-            ret, self.frame = self.cap.read()
+            ret, self.current_frame = self.video.read()
             if not ret:
                 break
-
-            results = self.model(self.frame, stream=True)
+            results = self.model(self.current_frame, stream=True)
 
             for r in results:
                 boxes = r.boxes
@@ -50,63 +56,63 @@ class NBAction:
 
                     conf = math.ceil((box.conf[0] * 100)) / 100
 
-                    cls = int(box.cls[0])
-                    current_class = self.class_names[cls]
+                    cclass = self.classes[int(box.cls[0])]
 
                     center = (int(x1 + w / 2), int(y1 + h / 2))
 
-
-                    if (conf > 0.2 or (in_hoop_region(center, self.hoop_pos) and conf > 0.1)) and current_class == "Basketball":
-                        self.ball_pos.append((center, self.frame_count, w, h, conf))
+                    if (conf > 0.2 or (within_shot_radius(center, self.hoop) and conf > 0.1)) and cclass == "Basketball":
+                        self.ball.append((center, self.total, w, h, conf))
                         x2, y2 = x1 + w, y1 + h
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 255), thickness=2)
-                        cv2.putText(self.frame, f"Basketball ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 0, 255), 1, lineType=cv2.LINE_AA)
+                        cv2.rectangle(self.current_frame, (x1, y1), (x2, y2), (0, 0, 255), thickness=2)
+                        cv2.putText(self.current_frame, f"Basketball ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 0, 255), 1, lineType=cv2.LINE_AA)
 
-                    if conf > 0.5 and current_class == "Basketball Hoop":
-                        self.hoop_pos.append((center, self.frame_count, w, h, conf))
+                    if conf > 0.5 and cclass == "Basketball Hoop":
+                        self.hoop.append((center, self.total, w, h, conf))
                         x2, y2 = x1 + w, y1 + h
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 255, 0), thickness=2)
-                        cv2.putText(self.frame, f"Basketball Hoop ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 255, 0), 1, lineType=cv2.LINE_AA)
+                        cv2.rectangle(self.current_frame, (x1, y1), (x2, y2), (255, 55, 174), thickness=2)
+                        cv2.putText(self.current_frame, f"Basketball Hoop ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 255, 0), 1, lineType=cv2.LINE_AA)
                         
-                    if conf > 0.5 and current_class == "Defence":
+                    if conf > 0.5 and cclass == "Defence":
                         x2, y2 = x1 + w, y1 + h
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 0), thickness=2)
-                        cv2.putText(self.frame, f"Defence ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+                        cv2.rectangle(self.current_frame, (x1, y1), (x2, y2), (0, 0, 0), thickness=2)
+                        cv2.putText(self.current_frame, f"Defence ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 0, 0), 1, lineType=cv2.LINE_AA)
                         
-                    if conf > 0.2 and current_class == "Player":
+                    if conf > 0.2 and cclass == "Player":
                         x2, y2 = x1 + w, y1 + h
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 152, 248), thickness=2)
-                        cv2.putText(self.frame, f"Player ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 152, 248), 1, lineType=cv2.LINE_AA)
+                        cv2.rectangle(self.current_frame, (x1, y1), (x2, y2), (0, 152, 248), thickness=2)
+                        cv2.putText(self.current_frame, f"Player ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (0, 152, 248), 1, lineType=cv2.LINE_AA)
                         
-                    if conf > 0 and current_class == "shooting":
-                        self.hoop_pos.append((center, self.frame_count, w, h, conf))
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
-                        cv2.putText(self.frame, f"Shooting ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (255,0,0), 1, lineType=cv2.LINE_AA)
-                        
-            self.clean_motion()
-            self.shot_detection()
-            self.display_score()
-            self.frame_count += 1
+                    if conf > 0 and cclass == "shooting":
+                        self.hoop.append((center, self.total, w, h, conf))
+                        cv2.rectangle(self.current_frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
+                        cv2.putText(self.current_frame, f"Shooting ({conf:.2f})", (x1, y1 - 10), self.font, 0.6, (255,0,0), 1, lineType=cv2.LINE_AA)
+            
+            #Run state functions every current_frame
+            self.update_state()
+            #Increment frame
+            self.total += 1
 
-            cv2.imshow('NBAction', self.frame)
-
+            cv2.imshow('NBAction', self.current_frame)
+            #Adjust how many current_frames program halts for, Q to exit window.
             if cv2.waitKey(1) & 0xFF == ord('q'):  
                 break
 
-        self.cap.release()
+        self.video.release()
         cv2.destroyAllWindows()
 
-    def clean_motion(self):
-        self.ball_pos = clean_ball_pos(self.ball_pos, self.frame_count)
-        for i in range(len(self.ball_pos)):
-            cv2.circle(self.frame, self.ball_pos[i][0], 2, (0, 0, 255), 2) 
 
-        if len(self.hoop_pos) > 0:
-            self.hoop_pos = clean_hoop_pos(self.hoop_pos)
-            hoop_center = self.hoop_pos[-1][0]
+    #Keep track of most recent ball and hoop locations,
+    def stabilize(self):
+        self.ball = stabilize_ball(self.ball, self.total)
+        for i in range(len(self.ball)):
+            cv2.circle(self.current_frame, self.ball[i][0], 2, (0, 0, 255), 2) 
+
+        if len(self.hoop) > 0:
+            self.hoop = stabilize_hoop(self.hoop)
+            hoop_center = self.hoop[-1][0]
             
-            hoop_width = self.hoop_pos[-1][2]
-            hoop_height = self.hoop_pos[-1][3]
+            hoop_width = self.hoop[-1][2]
+            hoop_height = self.hoop[-1][3]
             
             hoop_radius = int((hoop_width + hoop_height) / 4)  
 
@@ -114,54 +120,63 @@ class NBAction:
             
             hoop_radius = min(hoop_radius, max_hoop_radius)
 
-            cv2.circle(self.frame, hoop_center, hoop_radius, (128, 128, 0), 2) 
+            cv2.circle(self.current_frame, hoop_center, hoop_radius, (0, 255, 0), 2) 
 
-    def shot_detection(self):
-        if len(self.hoop_pos) > 0 and len(self.ball_pos) > 0:
-            if self.frame_count - self.last_attempt_frame >= self.cooldown_frames:
+    def check_score(self):
+        if len(self.hoop) > 0 and len(self.ball) > 0:
+            if self.total - self.last_attempt_current_frame >= self.cooldown_current_frames:
                 if score(self) and not self.ball_in_hoop:
                     self.shots_made += 1  
                     self.overlay_color = (0, 255, 0)
-                    self.fade_counter = self.fade_frames
+                    self.frame_count = self.revert_frames
 
                     self.ball_in_hoop = True
+                    self.last_attempt_current_frame = self.total  
 
-                    self.last_attempt_frame = self.frame_count 
+                    self.show_score_text = True
+                    self.score_text_total = 250 
+
                 elif not score(self):
                     self.ball_in_hoop = False
                         
     def display_score(self):
         text = "Shots Scored: " + str(self.shots_made)
-        frame_height, frame_width, _ = self.frame.shape
-
+        current_frame_height, current_frame_width, _ = self.current_frame.shape
+        
         text_size = cv2.getTextSize(text, self.font, 2, 5)[0] 
-        text_x = (frame_width - text_size[0]) // 2 
-        text_y = frame_height - 30 
+        text_x = (current_frame_width - text_size[0]) // 2 
+        text_y = current_frame_height - 30 
 
-        cv2.putText(self.frame, text, (text_x, text_y), self.font, 2, (0, 200, 252), 9, lineType=cv2.LINE_AA)  
-        cv2.putText(self.frame, text, (text_x, text_y), self.font, 2, (255, 0, 0), 4, lineType=cv2.LINE_AA)  
+        cv2.putText(self.current_frame, text, (text_x, text_y), self.font, 2, (0, 200, 252), 9, lineType=cv2.LINE_AA)  
+        cv2.putText(self.current_frame, text, (text_x, text_y), self.font, 2, (255, 0, 0), 4, lineType=cv2.LINE_AA)  
 
         if self.show_score_text:
             self.display_text("Score!")
-            self.score_text_frame_count -= 1
-            if self.score_text_frame_count <= 0:
-                self.show_score_text = False
+            self.score_text_total -= 1
+            if self.score_text_total <= 0:
+                self.show_score_text = False  
 
-        if self.fade_counter > 0:
-            alpha = 0.2 * (self.fade_counter / self.fade_frames)
-            self.frame = cv2.addWeighted(self.frame, 1 - alpha, np.full_like(self.frame, self.overlay_color), alpha, 0)
-            self.fade_counter -= 1
+        if self.frame_count > 0:
+            alpha = 0.2 * (self.frame_count / self.revert_frames)
+            self.current_frame = cv2.addWeighted(self.current_frame, 1 - alpha, np.full_like(self.current_frame, self.overlay_color), alpha, 0)
+            self.frame_count -= 1
 
+    def update_state(self):
+        self.stabilize()
+        self.check_score()
+        self.display_score()
+        
     def display_text(self, text):
-        frame_height, frame_width, _ = self.frame.shape
+        current_frame_height, current_frame_width, _ = self.current_frame.shape
 
         text_size = cv2.getTextSize(text, self.font, 2, 6)[0]
         text_width, text_height = text_size
 
-        text_x = (frame_width - text_width) // 2
-        text_y = (frame_height + text_height) // 2
-        cv2.putText(self.frame, text, (text_x, text_y-100), self.font, 2, (0, 255, 0), 6, lineType=cv2.LINE_AA)
+        text_x = (current_frame_width - text_width) // 2
+        text_y = (current_frame_height + text_height) // 2
 
+        cv2.putText(self.current_frame, text, (text_x, text_y-400), self.font, 2, (0, 0, 0), 9, lineType=cv2.LINE_AA)
+        cv2.putText(self.current_frame, text, (text_x, text_y-400), self.font, 2, (0, 255, 0), 4, lineType=cv2.LINE_AA)
 
 if __name__ == "__main__":
     NBAction()
--- NBAction/processing.py
+++ NBAction/processing.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def score(self):
 
-    if len(self.hoop_pos) == 0 or len(self.ball_pos) == 0:
+    if len(self.hoop) == 0 or len(self.ball) == 0:
         return False
 
-    hoop_center = self.hoop_pos[-1][0] 
-    hoop_radius = int(max(self.hoop_pos[-1][2], self.hoop_pos[-1][3]) * 0.5)  
-    ball_center = self.ball_pos[-1][0] 
+    hoop_center = self.hoop[-1][0] 
+    hoop_radius = int(max(self.hoop[-1][2], self.hoop[-1][3]) * 0.5)  
+    ball_center = self.ball[-1][0] 
 
     distance = math.sqrt((ball_center[0] - hoop_center[0]) ** 2 + (ball_center[1] - hoop_center[1]) ** 2)
 
@@ -18,88 +18,86 @@ def score(self):
 
     return False
 
-def detect_down(ball_pos, hoop_pos):
-    y = hoop_pos[-1][0][1] + 0.5 * hoop_pos[-1][3]
-    if ball_pos[-1][0][1] > y:
+def detect_down(ball, hoop):
+    y = hoop[-1][0][1] + 0.5 * hoop[-1][3]
+    if ball[-1][0][1] > y:
         return True
     return False
 
-def detect_up(ball_pos, hoop_pos):
-    x1 = hoop_pos[-1][0][0] - 4 * hoop_pos[-1][2]
-    x2 = hoop_pos[-1][0][0] + 4 * hoop_pos[-1][2]
-    y1 = hoop_pos[-1][0][1] - 2 * hoop_pos[-1][3]
-    y2 = hoop_pos[-1][0][1]
+def detect_up(ball, hoop):
+    x1 = hoop[-1][0][0] - 4 * hoop[-1][2]
+    x2 = hoop[-1][0][0] + 4 * hoop[-1][2]
+    y1 = hoop[-1][0][1] - 2 * hoop[-1][3]
+    y2 = hoop[-1][0][1]
 
-    if x1 < ball_pos[-1][0][0] < x2 and y1 < ball_pos[-1][0][1] < y2 - 0.5 * hoop_pos[-1][3]:
+    if x1 < ball[-1][0][0] < x2 and y1 < ball[-1][0][1] < y2 - 0.5 * hoop[-1][3]:
         return True
     return False
 
-def in_hoop_region(center, hoop_pos):
-    if len(hoop_pos) < 1:
+def within_shot_radius(center, hoop):
+    if len(hoop) < 1:
         return False
     x = center[0]
     y = center[1]
 
-    x1 = hoop_pos[-1][0][0] - 1 * hoop_pos[-1][2]
-    x2 = hoop_pos[-1][0][0] + 1 * hoop_pos[-1][2]
-    y1 = hoop_pos[-1][0][1] - 1 * hoop_pos[-1][3]
-    y2 = hoop_pos[-1][0][1] + 0.5 * hoop_pos[-1][3]
+    x1 = hoop[-1][0][0] - 1 * hoop[-1][2]
+    x2 = hoop[-1][0][0] + 1 * hoop[-1][2]
+    y1 = hoop[-1][0][1] - 1 * hoop[-1][3]
+    y2 = hoop[-1][0][1] + 0.5 * hoop[-1][3]
 
     if x1 < x < x2 and y1 < y < y2:
         return True
     return False
 
 
-def clean_ball_pos(ball_pos, frame_count):
+def stabilize_ball(ball, frame_count):
     #Upper limit on recent ball positions (the red dots)
     #This is here to avoid some glitches where dots appear all over the screen when the ball travels too quickly and disappears offscreen
     max_ball_count = 30
-    if len(ball_pos) > max_ball_count:
+    if len(ball) > max_ball_count:
         return []  # Reset all ball positions if limit exceeded
 
-    if len(ball_pos) > 1:
-        w1 = ball_pos[-2][2]
-        h1 = ball_pos[-2][3]
-        w2 = ball_pos[-1][2]
-        h2 = ball_pos[-1][3]
+    if len(ball) > 1:
+        w1 = ball[-2][2]
+        h1 = ball[-2][3]
+        w2 = ball[-1][2]
+        h2 = ball[-1][3]
 
-        x1 = ball_pos[-2][0][0]
-        y1 = ball_pos[-2][0][1]
-        x2 = ball_pos[-1][0][0]
-        y2 = ball_pos[-1][0][1]
+        x1 = ball[-2][0][0]
+        y1 = ball[-2][0][1]
+        x2 = ball[-1][0][0]
+        y2 = ball[-1][0][1]
 
-        f1 = ball_pos[-2][1]
-        f2 = ball_pos[-1][1]
+        f1 = ball[-2][1]
+        f2 = ball[-1][1]
         f_dif = f2 - f1
 
         dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
         max_dist = 4 * math.sqrt((w1) ** 2 + (h1) ** 2)
-
-        # Check for glitches: sudden large distance, mismatched aspect ratio
+        #Anomaly checker -- look for large distance jumps and remove
         if (dist > max_dist and f_dif < 5) or (w2 * 1.4 < h2) or (h2 * 1.4 < w2):
-            ball_pos.pop()
+            ball.pop()
 
-    # Remove stale ball positions
-    if len(ball_pos) > 0 and frame_count - ball_pos[0][1] > 5:
-        ball_pos.pop(0)
+    if len(ball) > 0 and frame_count - ball[0][1] > 5:
+        ball.pop(0)
 
-    return ball_pos
+    return ball
 
-def clean_hoop_pos(hoop_pos):
-    if len(hoop_pos) > 1:
-        x1 = hoop_pos[-2][0][0]
-        y1 = hoop_pos[-2][0][1]
-        x2 = hoop_pos[-1][0][0]
-        y2 = hoop_pos[-1][0][1]
+def stabilize_hoop(hoop):
+    if len(hoop) > 1:
+        x1 = hoop[-2][0][0]
+        y1 = hoop[-2][0][1]
+        x2 = hoop[-1][0][0]
+        y2 = hoop[-1][0][1]
 
-        w1 = hoop_pos[-2][2]
-        h1 = hoop_pos[-2][3]
-        w2 = hoop_pos[-1][2]
-        h2 = hoop_pos[-1][3]
+        w1 = hoop[-2][2]
+        h1 = hoop[-2][3]
+        w2 = hoop[-1][2]
+        h2 = hoop[-1][3]
 
-        f1 = hoop_pos[-2][1]
-        f2 = hoop_pos[-1][1]
+        f1 = hoop[-2][1]
+        f2 = hoop[-1][1]
 
         f_dif = f2-f1
 
@@ -108,12 +106,12 @@ def clean_hoop_pos(hoop_pos):
         max_dist = 0.5 * math.sqrt(w1 ** 2 + h1 ** 2)
 
         if dist > max_dist and f_dif < 5:
-            hoop_pos.pop()
+            hoop.pop()
 
         if (w2*1.3 < h2) or (h2*1.3 < w2):
-            hoop_pos.pop()
+            hoop.pop()
 
-    if len(hoop_pos) > 25:
-        hoop_pos.pop(0)
+    if len(hoop) > 25:
+        hoop.pop(0)
 
-    return hoop_pos
+    return hoop
