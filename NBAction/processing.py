578ae815dee036baeb45e1fce2d9b7e5f4b9106e
lin-simon <lin.simon2003@gmail.com>
2024-11-25 23:57:54 -0500

    optimize formula

--- NBAction/processing.py
+++ NBAction/processing.py
@@ -1,6 +1,22 @@
 import math
 import numpy as np
 
+
+def within_shot_radius(center, hoop):
+    #Checks if a point is within the shot radius of the hoop.
+    if len(hoop) < 1:
+        return False
+    x = center[0]
+    y = center[1]
+    #Get the center of the ball and hoop
+    x1 = hoop[-1][0][0] - 1 * hoop[-1][2]
+    x2 = hoop[-1][0][0] + 1 * hoop[-1][2]
+    y1 = hoop[-1][0][1] - 1 * hoop[-1][3]
+    y2 = hoop[-1][0][1] + 0.5 * hoop[-1][3]
+    #Check if the point is within the hoops bounds
+    if x1 < x < x2 and y1 < y < y2:
+        return True
+    
 def in_hoop(self):
     #Check if the ball is inside the net. -- True if a successful shot
     #We make sure at least one hoop and ball is in the current frame
@@ -17,25 +33,38 @@ def in_hoop(self):
     #Check if the ball is within the hoop radius and below the hoop's center. (a score)
     if distance <= hoop_radius and ball_center[1] > hoop_center[1]:
         return True
+    
+def stabilize_hoop(hoop):
+    #Same method but for the hoop,
+    if len(hoop) > 1:
+        x1 = hoop[-2][0][0]
+        y1 = hoop[-2][0][1]
+        x2 = hoop[-1][0][0]
+        y2 = hoop[-1][0][1]
+        w1 = hoop[-2][2]
+        h1 = hoop[-2][3]
+        w2 = hoop[-1][2]
+        h2 = hoop[-1][3]
 
-    return False
+        f1 = hoop[-2][1]
+        f2 = hoop[-1][1]
 
-def within_shot_radius(center, hoop):
-    #Checks if a point is within a shot radius near the hoop.
-    if len(hoop) < 1:
-        return False
-    x = center[0]
-    y = center[1]
-    #Get the center of the ball and hoop
-    x1 = hoop[-1][0][0] - 1 * hoop[-1][2]
-    x2 = hoop[-1][0][0] + 1 * hoop[-1][2]
-    y1 = hoop[-1][0][1] - 1 * hoop[-1][3]
-    y2 = hoop[-1][0][1] + 0.5 * hoop[-1][3]
-    #Check if the point is within the hoops bounds
-    if x1 < x < x2 and y1 < y < y2:
-        return True
-    return False
+        f_dif = f2-f1
 
+        distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        #Calculate the Euclidean distance between the last two hoop positions
+        max_dist = 0.5 * math.sqrt((w1 ** 2) + (h1 ** 2))
+        #Factor here is a lot smaller since hoops dont typically move (I hope)
+        if distance > max_dist and f_dif < 5:
+            hoop.pop()
+        #Remove if distance is too large -- an anomaly.
+        if (w2*1.3 < h2) or (h2*1.3 < w2):
+            hoop.pop()
+    #Remove old hoop positions for performance.
+    if len(hoop) > 25:
+        hoop.pop(0)
+
+    return hoop
 
 def stabilize_ball(ball, frame_count):
     #Upper limit on recent ball positions (the red dots)
@@ -67,39 +96,9 @@ def stabilize_ball(ball, frame_count):
         if (dist > max_dist and f_dif < 5) or (w2 * 1.4 < h2) or (h2 * 1.4 < w2):
             ball.pop()
     #Remove old ball positions for performance
-    if len(ball) > 0 and frame_count - ball[0][1] > 5:
+    if len(ball) > 0 and frame_count - ball[0][1] > 10:
         ball.pop(0)
 
     return ball
 
-def stabilize_hoop(hoop):
-    #Same method but for the hoop,
-    if len(hoop) > 1:
-        x1 = hoop[-2][0][0]
-        y1 = hoop[-2][0][1]
-        x2 = hoop[-1][0][0]
-        y2 = hoop[-1][0][1]
-        w1 = hoop[-2][2]
-        h1 = hoop[-2][3]
-        w2 = hoop[-1][2]
-        h2 = hoop[-1][3]
 
-        f1 = hoop[-2][1]
-        f2 = hoop[-1][1]
-
-        f_dif = f2-f1
-
-        dist = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-        #Calculate the Euclidean distance between the last two hoop positions
-        max_dist = 0.5 * math.sqrt(w1 ** 2 + h1 ** 2)
-        #Factor here is a lot smaller since hoops dont typically move (I hope)
-        if dist > max_dist and f_dif < 5:
-            hoop.pop()
-        #Remove if distance is too large -- an anomaly.
-        if (w2*1.3 < h2) or (h2*1.3 < w2):
-            hoop.pop()
-    #Remove old hoop positions for performance.
-    if len(hoop) > 25:
-        hoop.pop(0)
-
-    return hoop
