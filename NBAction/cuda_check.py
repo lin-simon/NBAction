c53b838b78ec5faf79af9918e8dfd5463b88334f
lin-simon <lin.simon2003@gmail.com>
2024-11-26 02:00:56 -0500

    add note

--- NBAction/cuda_check.py
+++ NBAction/cuda_check.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 #for checking if CUDA compatible for faster training
+#*this file is not necessary for NBAction to function*
 print("PyTorch version:", torch.__version__)
 print("Torchvision version:", torchvision.__version__)
 print("CUDA available:", torch.cuda.is_available())
